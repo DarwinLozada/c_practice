38b9c5b4999828f15207d2ff9645a2cdb7e273a0
darwin <darwinlozadadev@gmail.com>
2022-04-21 15:26:23 -0400

    Terminado ahora si

--- parcial1ejercicio1.c
+++ parcial1ejercicio1.c
@@ -8,27 +8,27 @@ int main (void) {
     int intentos = 0;
 
     // La clave por defecto es 1234.
-    int clave_default = 1234;
+    int clave_usuario = 1234;
     int clave_ingresada;
 
-    printf("Bienvenido al Sistema del Banco USM! \n\n");
+    printf("\nBienvenido al Sistema del Banco USM! \n\n");
 
     while (intentos <= maximo_intentos) {
         printf("Por favor ingrese su clave numerica: ");
         scanf("%d", &clave_ingresada);
         printf("\n");
 
-        if (clave_default != clave_ingresada) {
+        if (clave_usuario != clave_ingresada) {
             printf("Clave Incorrecta :( \n");
             intentos += 1;
         }
 
-        if (clave_default == clave_ingresada) {
+        if (clave_usuario == clave_ingresada) {
             break;
         }
     };
 
-    if (clave_default == clave_ingresada) {
+    if (clave_usuario == clave_ingresada) {
 
         printf("-------------------------------------------------");
         printf("\nBienvenido Usuario!\n");
@@ -44,13 +44,14 @@ int main (void) {
             printf("1- Consulta de saldo\n");
             printf("2- Retiro de dinero\n");
             printf("3- Deposito de dinero\n");
-            printf("4- Salir del sistema\n\n");
+            printf("4- Cambiar clave\n");
+            printf("5- Salir del sistema\n\n");
 
             scanf("%d", &opcion_elegida);
             printf("\n");
 
             if (opcion_elegida == 1) {
-                printf("-> Este es su saldo actual: %d Bs\n", dinero_en_la_cuenta);
+                printf("-> Este es su saldo actual: %d Bs\n\n", dinero_en_la_cuenta);
             }
 
             else if (opcion_elegida == 2) {
@@ -61,11 +62,11 @@ int main (void) {
                 printf("\n");
 
                 if (monto_a_retirar > dinero_en_la_cuenta) {
-                    printf("-> Disculpe, ese valor supera el monto que posee en su cuenta \n");
+                    printf("-> Disculpe, ese valor supera el monto que posee en su cuenta \n\n");
 
                 } else {
                     dinero_en_la_cuenta -= monto_a_retirar;
-                    printf("-> Dinero retirado con exito. Saldo actual: %d Bs\n", dinero_en_la_cuenta);
+                    printf("-> Dinero retirado con exito. Saldo actual: %d Bs\n\n", dinero_en_la_cuenta);
                 }
             }
 
@@ -77,14 +78,35 @@ int main (void) {
                 printf("\n");
 
                 if (monto_a_depositar < 0) {
-                    printf("-> No puede depositar una cantidad negativa\n");
+                    printf("-> No puede depositar una cantidad negativa\n\n");
                 } else {
                     dinero_en_la_cuenta += monto_a_depositar;
-                    printf("-> Dinero ingresado en su cuenta con exito! Saldo actual: %d Bs\n", dinero_en_la_cuenta);
+                    printf("-> Dinero ingresado en su cuenta con exito! Saldo actual: %d Bs\n\n", dinero_en_la_cuenta);
                 }
             }
 
             else if (opcion_elegida == 4) {
+
+                int seguir_intentando = 1;
+                int nueva_clave;
+
+                while (seguir_intentando == 1) {
+                    printf("Por favor ingrese su nueve clave. Debe ser de 4 digitos: ");
+                    scanf("%d", &nueva_clave);
+                    printf("\n");
+
+                    if (1000 <= nueva_clave && nueva_clave <= 9999) {
+                        clave_usuario = nueva_clave;
+                        printf("Clave cambiada con exito!\n\n");
+                        seguir_intentando = 0;
+
+                    } else {
+                        printf("Clave invalida...\n\n");
+                    }
+                }
+            }
+
+            else if (opcion_elegida == 5) {
                 printf("Muchas gracias, que tenga un lindo dia :) \n");
                 seguir_en_el_sistema = 0;
             }
